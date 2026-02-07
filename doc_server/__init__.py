1e18e000ba8f3e47b65f323dc3ec83e1c5f07a12
Artyom Katsap <artyom.katsap@gmail.com>
2026-02-07 21:37:11 +0200

    chore: Bump version to 0.1.0-alpha.4

--- doc_server/__init__.py
+++ doc_server/__init__.py
@@ -5,5 +5,5 @@ A comprehensive solution for ingesting, processing, and searching
 technical documentation with intelligent search capabilities.
 """
 
-__version__ = "0.1.0-alpha.3"
+__version__ = "0.1.0-alpha.4"
 __author__ = "Doc Server Team"
